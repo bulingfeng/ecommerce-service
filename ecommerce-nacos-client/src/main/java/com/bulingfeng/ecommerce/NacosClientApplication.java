72912a2d8469cb9c806943c331d7f63e2c89888c
bulingfeng <413957294@qq.com>
2022-07-07 15:38:16 +0800

    open-feign

--- ecommerce-nacos-client/src/main/java/com/bulingfeng/ecommerce/NacosClientApplication.java
+++ ecommerce-nacos-client/src/main/java/com/bulingfeng/ecommerce/NacosClientApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients // 开启openfeign功能注解
 @EnableDiscoveryClient
-@SpringBootApplication(scanBasePackages = "com.bulingfeng.ecommerce")
+@SpringBootApplication
 public class NacosClientApplication {
     public static void main(String[] args) {
         SpringApplication.run(NacosClientApplication.class,args);
